e5b319abebc9fe0f4b9d232345958129912e4f86
Devin Sharpe <devinrsharpe@gmail.com>
2022-07-18 23:25:24 -0400

    feat: add set-cookie header to successful login route

--- web/pages/api/auth/login.ts
+++ web/pages/api/auth/login.ts
@@ -1,6 +1,8 @@
-import { JSONSchemaType } from "ajv";
 import Route from "../../../utils/route";
+import { add } from "date-fns";
 import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import { serialize } from "cookie";
 
 const route = new Route();
 
@@ -18,7 +20,28 @@ route.post(
     if (user) {
       const isValid = await bcrypt.compare(req.body.password, user.password);
       if (isValid) {
-        res.send(user);
+        const userSession = await db.userSession.create({
+          data: {
+            userId: user.id,
+            expiresAt: add(new Date(), {
+              seconds: parseInt(process.env.SESSION_TTL || "3600", 10)
+            })
+          }
+        });
+        const jwt = jsonwebtoken.sign(
+          { session: userSession.id, exp: userSession.expiresAt.valueOf() },
+          process.env.JWT_SECRET || "secret"
+        );
+        res
+          .setHeader(
+            "Set-Cookie",
+            serialize("greenlog-auth-token", jwt, {
+              path: "/",
+              expires: userSession.expiresAt,
+              httpOnly: true
+            })
+          )
+          .send({ user: { ...user, password: undefined }, token: jwt });
       } else {
         res.status(401).send({
           error: "Invalid credentials",
