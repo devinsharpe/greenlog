899f5f9081261fd6b7ff36bc2144fe26c5bc3cb9
Devin Sharpe <devinrsharpe@gmail.com>
2022-07-18 20:37:28 -0400

    feat: create basic login route

--- web/pages/api/auth/login.ts
+++ web/pages/api/auth/login.ts
@@ -1,6 +1,47 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { JSONSchemaType } from "ajv";
+import Route from "../../../utils/route";
+import bcrypt from "bcrypt";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+const route = new Route();
+
+interface PostBody {
+  username: string;
+  password: string;
 }
+
+route.post(
+  async (req, res, { db }) => {
+    const { body } = req as { body: PostBody };
+    const user = await db.user.findFirst({
+      where: { username: body.username.trim().toLowerCase() }
+    });
+    if (user) {
+      const isValid = await bcrypt.compare(req.body.password, user.password);
+      if (isValid) {
+        res.send(user);
+      } else {
+        res.status(401).send({
+          error: "Invalid credentials",
+          message: "An account with that email and password could not be found"
+        });
+      }
+    } else {
+      res.status(401).send({
+        error: "Invalid credentials",
+        message: "An account with that email and password could not be found"
+      });
+    }
+  },
+  {
+    body: {
+      type: "object",
+      properties: {
+        username: { type: "string" },
+        password: { type: "string" }
+      },
+      required: ["username", "password"]
+    }
+  }
+);
+
+export default route.handle;
